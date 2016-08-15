3461653a37eee7d6137827745f84f9f7925e501c
Dennis Zung <dennis.zung@itaas.dimensiondata.com>
2016-08-15 17:25:33 +1000

    Init commit.

---
+++ AntiVirusWebApi/Controllers/AVController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Web.Http;
+using nClam;
+
+namespace AntiVirusWebApi.Controllers
+{
+	[RoutePrefix("api/avscan")]
+	public class AVController
+		: ApiController
+	{
+		public AVController()
+		{
+		}
+
+		[HttpGet, Route("byfilepath")]
+		public IHttpActionResult ScanFile(string filePath)
+		{
+			// TODO: Use IOC to set server config
+			ClamClient clam = new ClamClient("localhost", 3310);
+			ClamScanResult scanResult = clam.ScanFileOnServer(filePath);
+
+			// TODO: Return proper structured object
+			switch (scanResult.Result)
+			{
+				case ClamScanResults.Clean:
+					return Ok("File is clean!");
+				case ClamScanResults.VirusDetected:
+					return Ok($"Warning! File is infected by: '{scanResult.InfectedFiles.First().VirusName}'");
+				case ClamScanResults.Error:
+					return BadRequest($"An error occured when scanning the file: '{scanResult.RawResult}'");
+				default:
+					return BadRequest($"Expected scan result: '{scanResult.Result}'");
+			}
+		}
+	}
+}
---
+++ AntiVirusWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AntiVirusWebApi.WebApiApplication" Language="C#" %>
