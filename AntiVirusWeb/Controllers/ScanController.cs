28180ffed3a222a350af16083e2130a25202acea
Venkatesh Ramalingam <venkatesh.ramalingam@itaas.dimensiondata.com>
2016-08-16 11:33:18 +1000

    Fixed namespace issue and code refactor.

--- AntiVirusWeb/Controllers/ScanController.cs
+++ AntiVirusWeb/Controllers/ScanController.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
-namespace FileScanner.Controllers
+namespace AntiVirusWeb.Controllers
 {
 	public class ScanController : Controller
 	{
